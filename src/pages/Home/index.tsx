9926074be4104e1c7885617014537e0d9a69cfc9
André <adrfelipe.sz@gmail.com>
2024-06-20 15:26:22 -0300

    feat: :sparkles: Create history page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,13 +15,28 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput
+            id="task"
+            list="tasks-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="tasks-suggestions">
+            <option value="Desenvolvimento de software" />
+            <option value="Desenvolvimento de aplicativos" />
+            <option value="Desenvolvimento de sites" />
+            <option value="Desenvolvimento de jogos" />
+            <option value="Desenvolvimento de software" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             id="minutesAmount"
             type="number"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
